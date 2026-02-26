f8c5f4fd1c5b421e52341e4361c626fe7cf809b9
morez <rmomeni693@gmail.com>
2026-02-26 21:23:50 +0330

    design menu

--- 013_cipher_tool/solution.py
+++ 013_cipher_tool/solution.py
@@ -12,7 +12,9 @@ from rich.pretty import pprint
 from rich.panel import Panel
 from rich.tree import Tree
 from rich import print 
+from rich.console import Console
 import os
+console = Console()
 
 c = caesar.Caesar()
 a = atbash.Atbash()
@@ -23,33 +25,31 @@ while True:
     menu_tree.add("2.Atbash")
     menu_tree.add("3.Vigenere")
     menu_tree.add("4.Exit")
-    print(Panel(menu_tree))
-    choise = int(input("Enter your choise: ").strip())
+    print(Panel(menu_tree,title="[red]Menu"))
+    choise = int(console.input("Enter [i]your[/i] [bold red]choise[/]: ").strip())
     match choise:
         case 1 :
             while True:
-                pprint("Caesar")
                 caesar_tree = Tree("Algorithms")
                 caesar_tree.add("1.Encode")
                 caesar_tree.add("2.Decode")
                 caesar_tree.add("3.Change shift")
                 caesar_tree.add("4.Main Menu")
-                print(Panel(caesar_tree))
-                user_choise = int(input("Enter your choise: ").strip())
+                print(Panel(caesar_tree,title="[red]Caesar"))
+                user_choise = int(console.input("Enter [i]your[/i] [bold red]choise[/]: ").strip())
                 if user_choise == 1:
-                    user_string = input("Enter string: ").strip()
+                    user_string = console.input("Enter [i]your[/i] [bold red]string[/]: ").strip()
                     result = c.encode(user_string)
-                    print(f"{user_string} ---> {result}")
-                
+                    print(Panel(f"[green]{user_string} [blue]--->[/] [red]{result}",title="Result"))
                 elif user_choise == 2:
-                    user_string = input("Enter your encoded string: ").strip()
+                    user_string = console.input("Enter [i]your[/i] [bold red]string[/]: ").strip()
                     result = c.decode(user_string)
-                    print(f"{user_string} ---> {result}")
+                    print(Panel(f"[green]{user_string} [blue]--->[/] [red]{result}",title="Result"))
                 
                 elif user_choise == 3:
-                    user_new_shift = int(input("Enter your new shift: ").strip())
+                    user_new_shift = int(console.input("Enter [i]your[/i] [bold red]shift[/]: ").strip())
                     c.change_shift(user_new_shift)
-                    print(f"Shift now change to ---> {user_new_shift}")
+                    print(Panel(f"[green]Shift now change to [blue]--->[/] [red]{user_new_shift}"))
                 
                 elif user_choise == 4:
                     os.system("cls")
@@ -60,26 +60,27 @@ while True:
                 
         case 2 :    
             while True:
-                pprint("Atbash")
                 atbash_tree = Tree("Algorithms")
                 atbash_tree.add("1.Encode")
                 atbash_tree.add("2.Decode")
                 atbash_tree.add("3.Main Menu")
-                print(Panel(atbash_tree))
-                user_choise = int(input("Enter your choise: ").strip())
+                print(Panel(atbash_tree,title="[red]Atbash"))
+                user_choise = int(console.input("Enter [i]your[/i] [bold red]choise[/]: ").strip())
                 if user_choise == 1:
-                    user_string = input("Enter string: ").strip()
+                    user_string = console.input("Enter [i]your[/i] [bold red]string[/]: ").strip()
                     result = a.encode(user_string)
-                    print(f"{user_string} ---> {result}")
+                    print(Panel(f"[green]{user_string} [blue]--->[/] [red]{result}",title="Result"))
                 
                 elif user_choise == 2:
-                    user_string = input("Enter your encoded string: ").strip()
+                    user_string = console.input("Enter [i]your[/i] [bold red]string[/]: ").strip()
                     result = a.decode(user_string)
-                    print(f"{user_string} ---> {result}")
+                    print(Panel(f"[green]{user_string} [blue]--->[/] [red]{result}",title="Result"))
                 
                 elif user_choise == 3:
                     os.system("cls")
                     break
+                else:
+                    pprint("Invalid")
         case 4 :
             exit()
         case _ :
